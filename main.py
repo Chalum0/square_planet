57e6e6d4ebc127065c3fc368164aba5ff519d055
Snappyink2 <raphaelzanatta24@proton.me>
2023-09-16 22:12:11 +0200

    Updated main.py and settings.py.

--- main.py
+++ main.py
@@ -5,20 +5,25 @@
 from math import degrees, cos, sin
 from itertools import combinations
 from assets.settings import *
+import assets.game
 import random
 import pygame
 import pickle
 import time
 
 pygame.init()
-pygame.display.set_caption("3D Maker")
-SCREEN = pygame.display.set_mode((SCREEN_X, SCREEN_Y), pygame.FULLSCREEN)
-SCREEN_X, SCREEN_Y = SCREEN.get_size()
-HALF_SCREEN_X, HALF_SCREEN_Y = (int(SCREEN_X / 2), int(SCREEN_Y / 2))
+pygame.display.set_caption("BITE")
+SCREEN = pygame.display.set_mode((SCREEN_X, SCREEN_Y))
 CLOCK = pygame.time.Clock()
 
 playing = True
 while playing:
+
+    # Get outside values
+    mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
+    cos_x, cos_y, sin_x, sin_y = None, None, None, None
+
+
 
     # EVENTS
     for event in pygame.event.get():
