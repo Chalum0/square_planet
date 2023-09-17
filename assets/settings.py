34458b9f6759ceed72e1db4b5cef60481d656bcf
Snappyink2 <raphaelzanatta24@proton.me>
2023-09-17 14:18:46 +0200

    Updated main.py and settings.py.

--- assets/settings.py
+++ assets/settings.py
@@ -1,3 +1,5 @@
+import math
+
 # Screen settings
 SCREEN_X, SCREEN_Y = (1080, 720)
 HALF_SCREEN_X, HALF_SCREEN_Y = (int(SCREEN_X/2), int(SCREEN_Y/2))
@@ -7,4 +9,9 @@ HALF_SCREEN_X, HALF_SCREEN_Y = (int(SCREEN_X/2), int(SCREEN_Y/2))
 PLAYER_START_POS = (0, 0, 0)
 PLAYER_FOV = 400
 PLAYER_SPEED_START = 3.0
-CAMERA_SENSIBILITY = (0.05/2) * 3
+CAMERA_SENSIBILITY = round((0.05/2) * (0.05/2) * 3, 7)
+
+
+# Other
+RD85 = math.radians(85)
+NRD85 = -RD85
