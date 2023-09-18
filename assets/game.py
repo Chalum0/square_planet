bcc58f938f877f2f1e99a90250cc13ca1f2e0f6d
Snappyink2 <raphaelzanatta24@proton.me>
2023-09-18 11:10:45 +0200

    Updated game.py (first working version).

--- assets/game.py
+++ assets/game.py
@@ -1,5 +1,6 @@
 from math import dist
 from assets.settings import *
+import pygame
 # import pygame
 
 
@@ -25,7 +26,8 @@ def get_point_pos(points: tuple or list, cos_x: float, cos_y: float, sin_x: floa
         transformed_point = (p_x * cos_y + p_z * sin_y,
                              p_x * (sin_x * sin_y) + p_y * cos_x - p_z * (sin_x * cos_y),
                              p_y * sin_x + p_z * (cos_x * cos_y) - p_x * (cos_x * sin_y))
-        vs_points.append((transformed_point, distance))
+        # vs_points.append((transformed_point, distance))
+        vs_points.append(transformed_point)
         tp2 = transformed_point[2]
         if tp2 > 0:  # coordinates in screen space, point = (x*fov/z, y*fov/z)
             p = (transformed_point[0] * fov / tp2,
@@ -40,8 +42,24 @@ class Game:
     def __init__(self):
         self.player = Player()
         self.len_points = 0
+        self.len_polygons = 1
         self.points = []
         self.polygons = []
+
+        for i in range(200):
+            self.create_quare()
+
+    def create_quare(self):
+        self.points.append((5, 10, 5))
+        self.points.append((10, 10, 5))
+        self.points.append((5, 10, 10))
+        self.points.append((10, 10, 10))
+        self.len_points += 4
+
+        self.polygons.append(([1*self.len_polygons - 1, 2*self.len_polygons - 1,
+                               4*self.len_polygons - 1, 3*self.len_polygons - 1],
+
+                              0, 4, (255, 0, 0)))
 
 
 class Player:
@@ -51,3 +69,19 @@ class Player:
         self.pos = list(PLAYER_START_POS)
         self.fov = PLAYER_FOV
         self.speed = PLAYER_SPEED_START
+
+    def controls(self, keys, dt):
+        if keys[pygame.K_d]:  # If the D key is pressed: go right
+            self.pos[0], self.pos[2] = calculate_new_xy((self.pos[0], self.pos[2]), -self.speed * dt, -self.camY)
+        if keys[pygame.K_q] or keys[pygame.K_a]:  # If the Q or A key is pressed: go left
+            self.pos[0], self.pos[2] = calculate_new_xy((self.pos[0], self.pos[2]), self.speed * dt, -self.camY)
+        if keys[pygame.K_z] or keys[pygame.K_w]:  # If the Z or W key is pressed: go forward
+            self.pos[0], self.pos[2] = calculate_new_xy((self.pos[0], self.pos[2]), self.speed * dt,
+                                                        -self.camY + math.radians(90))
+        if keys[pygame.K_s]:  # If the s key is pressed: go backward
+            self.pos[0], self.pos[2] = calculate_new_xy((self.pos[0], self.pos[2]), -self.speed * dt,
+                                                        -self.camY + math.radians(90))
+        if keys[pygame.K_SPACE]:
+            self.pos[1] -= self.speed * dt
+        if keys[pygame.K_LCTRL] or keys[pygame.K_LSHIFT]:
+            self.pos[1] += self.speed * dt
