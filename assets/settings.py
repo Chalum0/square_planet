80e4260a75df7ec30450c28e07d058c93f3594b0
Snappyink2 <raphaelzanatta24@proton.me>
2023-09-18 11:10:56 +0200

    Updated settings.py (first working version).

--- assets/settings.py
+++ assets/settings.py
@@ -1,8 +1,9 @@
 import math
 
 # Screen settings
-SCREEN_X, SCREEN_Y = (1080, 720)
+SCREEN_X, SCREEN_Y = (1920, 1080)
 HALF_SCREEN_X, HALF_SCREEN_Y = (int(SCREEN_X/2), int(SCREEN_Y/2))
+MAX_FPS = 1000
 
 
 # Player settings
@@ -22,3 +23,9 @@ def clip3d(p1, p2, fov):
     p2_0, p2_1, p2_2 = p2
     step = ((ZCD-p1_2)/(p2_2-p1_2))
     return (p1_0 + (p2_0 - p1_0) * step) * fov / ZCD + HALF_SCREEN_X, (p1_1 + (p2_1 - p1_1) * step) * fov / ZCD + HALF_SCREEN_Y
+
+
+def calculate_new_xy(old_xy, speed, angle_in_radians):
+    new_x = old_xy[0] + -(speed * math.cos(angle_in_radians))
+    new_y = old_xy[1] + (speed * math.sin(angle_in_radians))
+    return new_x, new_y
